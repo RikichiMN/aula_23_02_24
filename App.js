b7047350393963077255c0aa8944f603071e6868
RikichiMN <henrique.nepomuceno@etec.sp.gov.br>
2024-02-23 22:49:11 -0300

    aula props

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Props from './src/ex_2';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Props mensagem={'Teste'}/> 
+    <Props mensagem={'Outro teste'}/>     
+    <StatusBar style="auto" />
     </View>
   );
 }
