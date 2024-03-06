566771c75650d99270a43d6e8b3ecfd7fa25342d
RikichiMN <henrique.nepomuceno@etec.sp.gov.br>
2024-03-06 20:40:10 -0300

    aula 06/03

--- App.js
+++ App.js
@@ -1,36 +1,23 @@
-//import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet, Text } from 'react-native';
 
-import Atv_1 from './src/atividade_1';
+//import Atv_1 from './src/atividade_1';
+import Exemplo_3 from './src/ex_3';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Atv_1 />         
+            <Exemplo_3 />
+            <StatusBar style="auto" />      
         </View>
     );
 }
 
-const styles = StyleSheet.create({
+ const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundcolor: '#E3F2FD',
-      padding: 8,
+        flex: 1,
+        backgroundColor: '#000',
+        justifyContent: 'center',
+        padding: 18,
     },
-    paragraph: {
-      margin: 24,
-      fontSize: 18,
-      fontWeight: 'bold',
-      TextAlign: 'center',
-      color: '#1565C0',
-    },
-    image: {
-      width: '90%',
-      height: '15%',
-      margin: 10,
-    },
-}); 
-
-
-
+ });
---
+++ src/ex_2/styles.js
@@ -0,0 +1,24 @@
+const styles = StyleSheet.create({
+    //container: {
+      //flex: 1,
+      //justifyContent: 'center',
+      //backgroundcolor: '#E3F2FD',
+      //padding: 8,
+    //},
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        TextAlign: 'center',
+        color: '#1565C0',
+      },
+      image: {
+        width: '90%',
+        height: '15%',
+        margin: 10,
+      },
+  }); 
+  
+  
+  
+  
